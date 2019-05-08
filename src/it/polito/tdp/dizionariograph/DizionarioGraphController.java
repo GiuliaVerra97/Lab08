cce00818fa1a5995656c2e80e032c5378d1283ae
Giulia Verra <Giulia Verra@172.21.15.17>
2019-05-08 14:19:36 +0200

    Completati tutti i punti, manca il controller

--- src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,67 @@
 package it.polito.tdp.dizionariograph;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class DizionarioGraphController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtNumLettere;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private TextArea txtArea;
+
+	private Model model;
+
+    @FXML
+    void doGeneraGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTrovaGradoMax(MouseEvent event) {
+
+    }
+
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtNumLettere != null : "fx:id=\"txtNumLettere\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtArea != null : "fx:id=\"txtArea\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+    }
+    
+    
+    public void setModel(Model model) {
+    	this.model=model;
+    }
+    
+    
 }
+
--- src/it/polito/tdp/dizionariograph/model/Model.java
+++ src/it/polito/tdp/dizionariograph/model/Model.java
@@ -3,21 +3,119 @@ package it.polito.tdp.dizionariograph.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.graph.SimpleGraph;
+
+import it.polito.tdp.dizionariograph.db.WordDAO;
+
 public class Model {
 
+	private Graph<String, DefaultEdge> grafo;
+	private List<String> listaParole;	
+	
 	public void createGraph(int numeroLettere) {
+		grafo=new SimpleGraph<>(DefaultEdge.class);
+		WordDAO dao=new WordDAO();
+		this.listaParole=dao.getAllWordsFixedLength(numeroLettere);
+		
+		Graphs.addAllVertices(this.grafo, this.listaParole);
+		
+		int conta;
+		
+		for(String partenza:this.grafo.vertexSet()) {
+			
+			for(String arrivo: this.grafo.vertexSet()) {
+				conta=0;
+				
+				for(int i=0; i<partenza.length();i++) {
+					if(partenza.charAt(i)!=arrivo.charAt(i))
+						conta++;
+				}
+				if(conta==1)
+					this.grafo.addEdge(partenza, arrivo);
+					
+			}
+			
+		}
 
-		System.err.println("createGraph -- TODO");
+		//System.err.println("createGraph -- TODO");
 	}
 
 	public List<String> displayNeighbours(String parolaInserita) {
-
-		System.err.println("displayNeighbours -- TODO");
-		return new ArrayList<String>();
+		//System.err.println("displayNeighbours -- TODO");
+		
+		List<String> result=new ArrayList<String>();
+		WordDAO dao=new WordDAO();
+		this.listaParole=dao.getAllWordsFixedLength(parolaInserita.length());
+		
+		for(String arrivo: listaParole) {
+			int conta=0;
+			for(int i=0; i<parolaInserita.length();i++) {
+				if(parolaInserita.charAt(i)!=arrivo.charAt(i))
+					conta++;
+			}
+			if(conta==1)
+				result.add(arrivo);
+		}
+		
+		
+		return result;
+		
+	
+		
+		
+		
 	}
 
 	public int findMaxDegree() {
-		System.err.println("findMaxDegree -- TODO");
-		return -1;
+		//System.err.println("findMaxDegree -- TODO");
+		
+		int  max=0;
+		
+		for(String parola: this.grafo.vertexSet()) {
+			int grado=grafo.degreeOf(parola);
+			
+			if(grado>max) {
+				max=grado;
+			}
+			
+		}
+		
+		return max;
 	}
+
+	
+	
+	
+	
+	public Graph<String, DefaultEdge> getGrafo() {
+		return grafo;
+	}
+
+	public void setGrafo(Graph<String, DefaultEdge> grafo) {
+		this.grafo = grafo;
+	}
+
+	public List<String> getListaParole() {
+		return listaParole;
+	}
+
+	public void setListaParole(List<String> listaParole) {
+		this.listaParole = listaParole;
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
 }
